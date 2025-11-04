c8a4e5617b874333e0fe4d6fd171a98e71b77379
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-11-04 09:26:22 -0500

    Complete GET and GET by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,19 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if(book){
+        return res.json(book);
+    } else {
+        return res.status(404).json({message: "Book not found"})
+    }
+    
  });
   
 // Get book details based on author
