ba21a8e43ffa5ae5a477504ffbe4aa664c736796
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-11-05 08:10:57 -0500

    Add book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,10 +65,16 @@ public_users.get('/title/:title', function (req, res) {
     }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    
+    if (book) {
+        res.send(JSON.stringify(book.reviews, null, 4));
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.general = public_users;
