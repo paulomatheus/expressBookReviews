1d6d5772d80f55a50f9e0d5efea253c375648826
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-11-05 08:59:39 -0500

    Add PUT review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,8 +45,21 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization['username'];
+    
+    if (!review) {
+        return res.status(400).json({message: "Review text is required"});
+    }
+    
+    if (!books[isbn]) {
+        return res.status(404).json({message: "Book not found"});
+    }
+    
+    books[isbn].reviews[username] = review;
+    
+    return res.status(200).json({message: "Review successfully added/updated"});
 });
 
 module.exports.authenticated = regd_users;
